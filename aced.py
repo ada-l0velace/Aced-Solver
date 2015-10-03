825c93a4f53316f0bbbf49056107ba6c7cf4372e
lolstorm <ricardocampos65@hotmail.com>
2015-10-04 00:30:01 +0100

    Now performs path integrals and simple operations

--- aced.py
+++ aced.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 from w_requests import WolframRequests
 from prettytable import PrettyTable
 
+
 class BaseAced():
 	"""
 	Base Class for all Aced Classes
@@ -61,3 +62,65 @@ class Holomorph(BaseAced):
 		dvdx = self.wolfram_request[2].get_pod()[4].text
 		self.matrix_r([[dudx,dxdy],[dvdx,dvdy]])
 		self.answer = dudx == dvdy and dxdy == '-%s' % dvdx
+
+
+class SimpleOperation(BaseAced):
+	"""
+	Class to make simple operations
+	"""
+
+	def __init__(self, func, app_id):
+		self.func = func
+		self.querys = [func]
+		self.set_requests(app_id)
+		self.answer = False
+		self.value = []
+
+	def __str__(self):
+		if(self.answer):
+			return "The answer is %s" % self.value 
+		return "An error ocurred"
+
+	def run (self):
+		super(SimpleOperation,self).run()
+		self.value = self.wolfram_request[0].get_pod()[1].text
+		self.answer = True
+
+
+class PathIntegral(BaseAced):
+	"""
+	Class to perform path integrals
+	"""
+
+	def __init__(self, func, path, irange, app_id):
+		self.app_id = app_id
+		self.path = path
+		self.t = '%s at z=%s' % (func,path)
+		self.dt = 'd/dt(%s)' % path
+		self.integral = 'Integrate[(%s)+(%s), {t, %d, %d}]'
+		self.range = irange
+		self.func = func
+		self.querys = [self.t, self.dt]
+		self.set_requests(app_id)
+		self.answer = False
+		self.value = ""
+
+	def __str__(self):
+		if(self.answer):
+			return "%s" % self.value 
+		return "An error ocurred"
+
+	def run_last(self, t, dt):
+		r = WolframRequests(self.app_id)
+		r.run('Integrate[(%s)(%s), {t, %d, %d}]' % (t, dt, self.range[0],self.range[1]))
+		r.join()
+		self.value = "f(t) = %s\nf'(t) = %s\n%s" % (t,dt,r.get_pod()[0].text[1:])
+		self.answer = True
+		
+	def run (self):
+		super(PathIntegral,self).run()
+		t = self.wolfram_request[0].get_pod()[3].text
+		dt = self.wolfram_request[1].get_pod()[0].text.split(' = ')[1]
+		self.run_last(t,dt)
+		#self.value = self.wolfram_request[0].get_pod()[1].text
+		
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from aced import Holomorph
+from aced import Holomorph, SimpleOperation, PathIntegral
 
 APP_ID = '#'
 
 if __name__ == "__main__":
 	acedH = Holomorph('e^(x)cos(y)+ie^(x)sin(y)', APP_ID)
-	acedH.run()
-	print acedH
+	#acedH.run()
+	#print acedH
+	acedSF = SimpleOperation('series representations (1/((w+i+3)-(-4i-2)))', APP_ID)
+	acedPI = PathIntegral('conjugate(z)', '-(3i-2)t^(2)-(3i-1)t-i-2', [-2,1], APP_ID)
+	acedPI.run()
+	print acedPI
